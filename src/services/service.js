c74c4bcaecf127d0928d9eedadb31dd8e4737719
yushever <iushina.tatiana@gmail.comgit config --global user.email iushina.tatiana@gmail.comgit config --global user.email iushina.tatiana@gmail.com>
2022-09-19 17:44:56 +0300

    debounce is not working

--- src/services/service.js
+++ src/services/service.js
@@ -1,16 +1,6 @@
+import _ from 'lodash';
+
 export default class GetMovies {
-  debounce(func, delay = 1000) {
-    let timeoutId;
-
-    return async function (...args) {
-      clearTimeout(timeoutId);
-
-      timeoutId = setTimeout(() => {
-        func(...args);
-      }, delay);
-    };
-  }
-
   async getResource(url) {
     const res = await fetch(url);
 
@@ -21,16 +11,16 @@ export default class GetMovies {
     return body;
   }
 
-  debounced = this.debounce(this.getResource);
+  debounced = _.debounce(async (url) => await this.getResource(url), 2000, { leading: true });
 
   async getAllMovies(searchInput) {
     if (searchInput === null || searchInput === '') {
       return [];
     }
-    const res = await this.getResource(
+    const res = await this.debounced(
       `https://api.themoviedb.org/3/search/movie?api_key=f0f65e607fe0520c21899abdded8460f&query=${searchInput}&page=1`
     );
-    // console.log(res);
+    console.log(res);
 
     return res.results;
   }
